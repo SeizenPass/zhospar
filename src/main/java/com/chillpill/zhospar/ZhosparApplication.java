81b1eeee092c7fc06a8914e23322f4d35d2f3cb5
Amiran Kurman <amiranfox@gmail.com>
2021-04-26 22:18:34 +0600

    first commit

---
+++ src/main/java/com/chillpill/zhospar/ZhosparApplication.java
@@ -0,0 +1,13 @@
+package com.chillpill.zhospar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhosparApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhosparApplication.class, args);
+    }
+
+}
